40616ca0ef8d50f122f3fce7077afc63641a5f47
KAdamczyk <konrad1adamczyk@gmail.com>
2017-12-27 14:54:00 +0100

    poprawione

--- src/main/java/com/hellokoding/auth/WebApplication.java
+++ src/main/java/com/hellokoding/auth/WebApplication.java
@@ -1,18 +1,18 @@
 package com.hellokoding.auth;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@ComponentScan(basePackages = {"com.hellokoding.auth"})
 @SpringBootApplication
-public class WebApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(WebApplication.class);
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(WebApplication.class, args);
+@EnableAutoConfiguration
+@Configuration
+public class WebApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class);
     }
 }
